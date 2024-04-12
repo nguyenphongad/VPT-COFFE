7287a8c04e2ff7cd0cab4bc5c580ef3a9aea0a91
PHONG <cuphong2003hk@gmail.com>
2024-04-13 01:34:04 +0700

    update UI vu

--- SoureCode/VPTCoffee_Nhom08/src/ui/HoaDon_QuanLy_UI.java
+++ SoureCode/VPTCoffee_Nhom08/src/ui/HoaDon_QuanLy_UI.java
@@ -1,11 +1,186 @@
 package ui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
 public class HoaDon_QuanLy_UI extends JPanel{
+	private JTable tableHoaDon;
+	private DefaultTableModel modelHoaDon;
+	private JTable tableCTHoaDon;
+	private DefaultTableModel modelCTHoaDon;
+	private JScrollPane tableScroll;
+	private JLabel maHDValue;
+	private JLabel tenKHValue;
+	private JLabel sdtValue;
+	private JLabel ngayLapHDValue;
+	private JLabel loaiThanhToanValue;
+	private JTextArea ghiChuArea;
+	private JTextArea diaChiArea;
+	private JLabel tongTienSanPhamValue;
+	private JLabel tongTienHoaDonValue;
+	private JLabel chiPhiKhacValue;
+	
 	public HoaDon_QuanLy_UI() {
-		JLabel lblNewLabel = new JLabel("quan ly hoa don");
-		add(lblNewLabel);
+		GridLayout gridLayout_1 = new GridLayout(1, 2);
+		setLayout(gridLayout_1);
+		
+		JPanel danhSachHoaDonPnl = new JPanel();
+		danhSachHoaDonPnl.setBorder(new EmptyBorder(10, 10, 10, 10));
+		JPanel chiTietHoaDonPnl = new JPanel();
+		chiTietHoaDonPnl.setBorder(new EmptyBorder(10, 10, 10, 10));
+		
+		BorderLayout borderLayout_1 = new BorderLayout(5, 5);
+		danhSachHoaDonPnl.setLayout(borderLayout_1);
+		
+		JLabel danhSachHoaDonLabel = new JLabel("Danh Sách Hóa Đơn", SwingConstants.CENTER);
+		danhSachHoaDonLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
+		danhSachHoaDonPnl.add(borderLayout_1.NORTH, danhSachHoaDonLabel);
+		
+		String[] danhSachHoaDonColHeader = { "Mã HĐ", "Tên Khách Hàng", "Tên Nhân Viên", "Ngày Lập HĐ", "Tổng tiền"};
+		modelHoaDon = new DefaultTableModel(danhSachHoaDonColHeader, 0);
+		tableHoaDon = new JTable(modelHoaDon);
+		tableScroll = new JScrollPane(tableHoaDon);
+		tableScroll.getViewport().setBackground(Color.white);
+		danhSachHoaDonPnl.add(tableScroll, BorderLayout.CENTER);
+		
+		BorderLayout borderLayout_2 = new BorderLayout(5, 5);
+		chiTietHoaDonPnl.setLayout(borderLayout_2);
+		
+		JLabel chiTietHoaDonLabel = new JLabel("Chi Tiết Hóa Đơn", SwingConstants.CENTER);
+		chiTietHoaDonLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
+		chiTietHoaDonPnl.add(borderLayout_2.NORTH, chiTietHoaDonLabel);
+		
+		String[] chiTietHoaDonColHeader = { "Tên SP", "Đơn Giá", "Thành Tiền", "Ghi Chú"};
+		modelCTHoaDon = new DefaultTableModel(chiTietHoaDonColHeader, 0);
+		tableCTHoaDon = new JTable(modelCTHoaDon);
+		tableScroll = new JScrollPane(tableCTHoaDon);
+		tableScroll.getViewport().setBackground(Color.white);
+		chiTietHoaDonPnl.add(tableScroll, BorderLayout.CENTER);
+		
+		JPanel noiDungChiTietHoaDonPnl = new JPanel();
+		
+		GridLayout gridLayout_2 = new GridLayout(1, 2);
+		noiDungChiTietHoaDonPnl.setLayout(gridLayout_2);
+		
+		JPanel leftColPanel = new JPanel();
+		BoxLayout boxLayout_1 = new BoxLayout(leftColPanel, BoxLayout.Y_AXIS);
+		leftColPanel.setLayout(boxLayout_1);
+		
+		JPanel rightColPanel = new JPanel();
+		BoxLayout boxLayout_2 = new BoxLayout(rightColPanel, BoxLayout.Y_AXIS);
+		rightColPanel.setLayout(boxLayout_2);
+		
+		JPanel maHDPanel = new JPanel();
+		maHDPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		JLabel maHDLabel = new JLabel("Mã Hóa Đơn:");
+		maHDValue = new JLabel("HD0003");
+		maHDValue.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+		maHDPanel.add(maHDLabel);
+		maHDPanel.add(maHDValue);
+		
+		JPanel tenKHPanel = new JPanel();
+		tenKHPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		JLabel tenKHLabel = new JLabel("Tên Khách Hàng:");
+		tenKHValue = new JLabel("Trần Đức Vũ");
+		tenKHValue.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+		tenKHPanel.add(tenKHLabel);
+		tenKHPanel.add(tenKHValue);
+		
+		JPanel sdtPanel = new JPanel();
+		sdtPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		JLabel sdtLabel = new JLabel("Số Điện Thoại:");
+		sdtValue = new JLabel("0946890234");
+		sdtValue.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+		sdtPanel.add(sdtLabel);
+		sdtPanel.add(sdtValue);
+		
+		JPanel ngayLapHDPanel = new JPanel();
+		ngayLapHDPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		JLabel ngayLapHDLabel = new JLabel("Ngày Lập HĐ:");
+		ngayLapHDValue = new JLabel("15:29 20-04-2033");
+		ngayLapHDValue.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+		ngayLapHDPanel.add(ngayLapHDLabel);
+		ngayLapHDPanel.add(ngayLapHDValue);
+		
+		JPanel ghiChuPanel = new JPanel();
+		ghiChuPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		JLabel ghiChuLabel = new JLabel("Ghi Chú:");
+		ghiChuArea = new JTextArea(5, 30);
+		ghiChuPanel.add(ghiChuLabel);
+		ghiChuPanel.add(ghiChuArea);
+		
+		leftColPanel.add(maHDPanel);
+		leftColPanel.add(tenKHPanel);
+		leftColPanel.add(sdtPanel);
+		leftColPanel.add(ngayLapHDPanel);
+		leftColPanel.add(ghiChuPanel);
+		
+		JPanel tongTienSanPhamPanel = new JPanel();
+		tongTienSanPhamPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		JLabel tongTienSanPhamLabel = new JLabel("Tổng tiền HĐ:");
+		tongTienSanPhamValue = new JLabel("20.300 đ");
+		tongTienSanPhamValue.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+		tongTienSanPhamPanel.add(tongTienSanPhamLabel);
+		tongTienSanPhamPanel.add(tongTienSanPhamValue);
+
+		JPanel tongTienHoaDonPanel = new JPanel();
+		tongTienHoaDonPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		JLabel tongTienHoaDonLabel = new JLabel("Tổng tiền SP:");
+		tongTienHoaDonValue = new JLabel("20.300đ");
+		tongTienHoaDonValue.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+		tongTienHoaDonPanel.add(tongTienHoaDonLabel);
+		tongTienHoaDonPanel.add(tongTienHoaDonValue);
+		
+		JPanel chiPhiKhacPanel = new JPanel();
+		chiPhiKhacPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		JLabel chiPhiKhacLabel = new JLabel("Chi phí khác:");
+		chiPhiKhacValue = new JLabel("0đ");
+		chiPhiKhacValue.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+		chiPhiKhacPanel.add(chiPhiKhacLabel);
+		chiPhiKhacPanel.add(chiPhiKhacValue);
+
+		JPanel loaiThanhToanPanel = new JPanel();
+		loaiThanhToanPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		JLabel loaiThanhToanLabel = new JLabel("Loại Thanh Toán:");
+		loaiThanhToanValue = new JLabel("Chuyển khoản");
+		loaiThanhToanValue.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+		loaiThanhToanPanel.add(loaiThanhToanLabel);
+		loaiThanhToanPanel.add(loaiThanhToanValue);
+		
+		JPanel diaChiPanel = new JPanel();
+		diaChiPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		JLabel diaChiLabel = new JLabel("Địa chỉ:");
+		diaChiArea = new JTextArea(5, 30);
+		diaChiPanel.add(diaChiLabel);
+		diaChiPanel.add(diaChiArea);
+		
+		rightColPanel.add(tongTienSanPhamPanel);
+		rightColPanel.add(chiPhiKhacPanel);
+		rightColPanel.add(tongTienHoaDonPanel);
+		rightColPanel.add(loaiThanhToanPanel);
+		rightColPanel.add(diaChiPanel);
+		
+		noiDungChiTietHoaDonPnl.add(leftColPanel);
+		noiDungChiTietHoaDonPnl.add(rightColPanel);
+		
+		chiTietHoaDonPnl.add(BorderLayout.SOUTH, noiDungChiTietHoaDonPnl);
+		
+		add(danhSachHoaDonPnl);
+		add(chiTietHoaDonPnl);
+		
+		
 	}
 }
--- SoureCode/VPTCoffee_Nhom08/src/ui/ThongKe_DoanhThu_UI.java
+++ SoureCode/VPTCoffee_Nhom08/src/ui/ThongKe_DoanhThu_UI.java
@@ -1,11 +1,145 @@
 package ui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.util.Calendar;
+
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 public class ThongKe_DoanhThu_UI extends JPanel {
+	private JPanel panelTKThang;
+	private JTextField tongDoanhThuTxt;
+	private JComboBox<Integer> thangComboBox;
+	private Double tongDoanhThuThang;
+	
 	public ThongKe_DoanhThu_UI() {
-		JLabel lblNewLabel = new JLabel("thong ke doanh thu");
-		add(lblNewLabel);
+		BorderLayout borderLayout = new BorderLayout();
+		setLayout(borderLayout);
+		setBackground(Color.white);
+		
+		JPanel northPanel = new JPanel();
+		northPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		northPanel.setLayout(new GridLayout(2, 1));
+		northPanel.setBackground(Color.white);
+		
+		JLabel thongKeDoanhThuLabel = new JLabel("Thống Kê Doanh Thu Theo Tháng", SwingConstants.CENTER);
+		thongKeDoanhThuLabel.setFont(new Font(Font.SERIF, Font.BOLD, 20));
+			
+		panelTKThang = new JPanel();
+		panelTKThang.setBackground(Color.white);
+		
+		JPanel thangPanel =  new JPanel();
+		thangPanel.setBackground(Color.white);
+		JLabel thangLabel = new JLabel("Chọn Tháng:");
+        Integer thang[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        thangComboBox = new JComboBox<Integer>(thang);
+        thangComboBox.setPreferredSize(new Dimension(50, 20));
+		thangPanel.add(thangLabel);
+		thangPanel.add(thangComboBox);
+		
+		JPanel tongDoanhThuPanel = new JPanel();
+		tongDoanhThuPanel.setBackground(Color.white);
+		JLabel tongDoanhThuLabel = new JLabel("Tổng Doanh Thu:");
+		tongDoanhThuTxt = new JTextField(10);
+		tongDoanhThuPanel.add(tongDoanhThuLabel);
+		tongDoanhThuPanel.add(tongDoanhThuTxt);
+		
+		JPanel controlPanel = new JPanel();
+		controlPanel.setBackground(Color.white);
+		controlPanel.setLayout(new GridLayout(1, 2));
+		controlPanel.add(thangPanel);
+		controlPanel.add(tongDoanhThuPanel);
+		
+		northPanel.add(thongKeDoanhThuLabel);
+		northPanel.add(controlPanel);
+		
+		// Create chart
+		thongKeThang();
+		
+		add(BorderLayout.NORTH, northPanel);
+		add(BorderLayout.CENTER, panelTKThang);
 	}
+	
+	  private JFreeChart showChartNull() {
+	        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+	        for (int i = 1; i < 31; i++) {
+	            String ngay = String.valueOf(i);
+	            long tt = 0;
+	            dataset.addValue(tt, "Tổng tiền", ngay);
+	        }
+	        JFreeChart barChart = ChartFactory.createBarChart("", "Ngày",
+	                "Tổng tiền (VND)", dataset, PlotOrientation.VERTICAL, true, true, false);
+	        
+	        CategoryPlot plot = (CategoryPlot) barChart.getPlot();
+	        NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+	        rangeAxis.setRange(0, 5000000);
+	        
+	        return barChart;
+	        
+	    }
+	  
+	  private void thongKeThang() {
+	        panelTKThang.revalidate();
+	        panelTKThang.repaint();
+	        panelTKThang.removeAll();
+	        
+//	        int soHoaDon = daoHoaDon.tongSoHoaDonTheoThang(Integer.parseInt(thangComboBox.getSelectedItem().toString()));
+	        int soHoaDon = 0; // Remove current row code after daoHoaDon class was created
+	        
+	        if (soHoaDon == 0) {
+	            ChartPanel chartPanelNull = new ChartPanel(showChartNull());
+	            chartPanelNull.setPreferredSize(new Dimension(1100, 480));
+	            panelTKThang.add(chartPanelNull);
+	        } else {
+		        ChartPanel chartPanel = new ChartPanel(createChartThang());
+		        tongDoanhThuTxt.setText(String.format("%,.0f" + " VNĐ", tongDoanhThuThang));
+		        panelTKThang.add(chartPanel);
+	        }   
+	  }
+	  
+	  private JFreeChart createChartThang() {
+	        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+	        int month = thangComboBox.getSelectedIndex();
+	        int n = 32;
+	        Calendar c = Calendar.getInstance();
+	        int currentYear = c.get(Calendar.YEAR);
+	        n = switch (month) {
+	            case 1 -> 32;
+	            case 2 -> ((currentYear % 4 == 0 || (currentYear % 100 != 0 && currentYear % 400 == 0)) && month == 2) ? 30 : 29;
+	            case 3 -> 32;
+	            case 5 -> 32;
+	            case 7 -> 32;
+	            case 8 -> 32;
+	            case 10 -> 32;
+	            case 12 -> 32;
+	            default -> 31;
+	        };
+	        double tt = 0;
+
+	        for (int i = 1; i < n; i++) {
+	            String ngay = String.valueOf(i);
+//	            tt = daoHoaDon.tongDoanhThuTheoTungNgayTrongThang(i, month);
+	            tongDoanhThuThang += tt;
+	            dataset.addValue(tt, "Tổng tiền", ngay);
+	        }
+	        JFreeChart barChart = ChartFactory.createBarChart("", "Ngày", "Tổng tiền (VND)", dataset, PlotOrientation.VERTICAL, false, false, false);
+	        return barChart;
+	    }
 }
